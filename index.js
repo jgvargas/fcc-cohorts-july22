65075d425f95f3430fbb80c90a4f76fbc8df1a37
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2022-08-05 20:44:56 -0500

    Navbar rework

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-}
+// Mobile nav menu 
 
-window.initMap = initMap;
+mobileMenu = document.getElementById('mobile-menu')
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
+mobileMenu.addEventListener('click', () => {
+  console.log("first")
+  mobileMenu.classList.toggle('active')
+})
